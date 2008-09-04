7ee0ef122617388130e9058add1103c37f3286ec
Shlomi Atar <shlomiatar@gmail.com>
2008-09-04 20:37:45 +0300

    added basis for editing operations

--- public/javascripts/nifty/widgets/field_panel.js
+++ public/javascripts/nifty/widgets/field_panel.js
@@ -25,6 +25,19 @@ Nifty.widgets.field = Ext.extend(Ext.DataView, {
 	 * will be used to construct the instance template
      * 
      */
+	/**
+     * @cfg {string} placement
+     * sets if the fieldlets will be place in a block mode (like list items)
+     * in an inline mode (like a seperated list - tag list for example)
+	 * in a table view 
+     */
+	/**
+     * @cfg {string} instanceTagName
+     * 
+     * 
+	 * 
+     */
+	
 	
 	// used with the form layout
 	isFormField: true,
@@ -34,13 +47,19 @@ Nifty.widgets.field = Ext.extend(Ext.DataView, {
 		// setup labels
 		this.fieldLabel = this.name;
 		
+		this.trackOver = true;
 		// hookup template and store
 
-		this.store = Ext.StoreMgr.lookup(this.fieldId);
+		this.store = Ext.StoreMgr.lookup(this.fieldId); //setup the store
+		
 		if(!this.children){this.children = Nifty.schema.loaded.elements[this.fieldId]};
 		
-		if(!this.seperatorFragment){this.seperatorFragment = '<br/>'};
+		if(!this.placement){this.placement = 'block'}
 		
+		if(!this.instanceTagName && this.placement === 'block'){this.instanceTagName = 'div'};
+		if(!this.instanceTagName && this.placement === 'inline'){this.instanceTagName = 'span'};		
+		if(!this.instanceTagName && this.placement === 'table'){this.instanceTagName = 'tr'};
+				
 		if(!this.instanceFragments){
 			this.instanceFragments = [];
 			Ext.each(this.children,function(child){
@@ -48,23 +67,86 @@ Nifty.widgets.field = Ext.extend(Ext.DataView, {
 			},this);
 			
 		}
-		this.itemSelector = 'span.x-nifty-field-instance';
-		this.tpl = new Ext.XTemplate(this.buildTemplate());
+		this.itemSelector = this.instanceTagName + '.x-nifty-field-instance';
 		
+		// speed up things with a little bit of caching
+		if(Nifty.cache.elements[this.identifier]){
+			this.tpl = Nifty.cache.elements[this.identifier];
+		} else {
+			this.tpl = Nifty.cache.elements[this.identifier] = new Ext.XTemplate(this.buildTemplate());
+		}
+				
 		// call the to superclass 
 		Nifty.widgets.field.superclass.initComponent.call(this);
+		
+		this.on('render', this.setupActionTab, this, {delay:20})
 	},
 	
+	setupActionTab: function(){
+		this.tabEl = Ext.get('nifty-tab-' + this.fieldId);
+		if(!this.tabEl){return};
+		this.tabEl.setVisibilityMode(Ext.Element.VISIBILITY);
+		
+		this.hookActionTab();
+	},
+
+
+	hookActionTab: function(){
+		this.on('mouseenter', this.tabEvents.mouseEnter, this, {stopPropagation: true});
+		this.tabEl.on('click',this.tabEvents.startEdit,this, {delegate: 'img.x-nifty-tab-edit'});
+		this.tabEl.on('click',this.tabEvents.deleteInstance,this, {delegate: 'img.x-nifty-tab-delete'});
+		this.el.on('mouseout', this.tabEvents.mouseOut, this)
+	},
+	
+	
+	
+	tabEvents: {
+		// called when the mouse enter
+		mouseEnter: function(dv, recordNumber, node, e){
+			if(this.tabEl.currentItem === recordNumber){return;}
+			this.tabEl.alignTo(node, 'tr-tl');
+			this.tabEl.currentItem = recordNumber;
+			this.tabEl.setVisible(true); // showit
+		},
+		
+		mouseOut: function(e){
+			t = e.getRelatedTarget();
+			if(!t){return;}
+			if(t.id == this.el.id){return;}
+			if(e.within(this.el,true)){return;}
+			
+			this.tabEl.setVisible(false);
+		},
+		
+		
+		startEdit: function(){
+			alert('editing ' + this.tabEl.currentItem);
+		},
+		
+		deleteInstance: function(){
+			alert('delete ' + this.tabEl.currentItem);
+		}
+	},
+	
+	buildActionTab: function(){
+		var items = [];
+		items.push(String.format('<img class="x-nifty-tab-delete" src="{0}"/>',Ext.BLANK_IMAGE_URL));
+		items.push(String.format('<img class="x-nifty-tab-edit" src="{0}"/>',Ext.BLANK_IMAGE_URL));
+
+		return items.join('');
+	},
 	
 	buildTemplate: function(){
 		var templateFragments = [];
 		
 		if(this.beforeFragment){templateFragments.push(this.beforeFragment)};
 		
-		templateFragments.push('<tpl for="."><span class="x-nifty-field-instance">');
-		templateFragments.push('<tpl if="xindex &gt; 1">');
-		templateFragments.push(this.seperatorFragment);
-		templateFragments.push('</tpl>');
+		templateFragments.push(String.format('<tpl for="."><{0} class="x-nifty-field-instance">', this.instanceTagName));
+		if(this.seperatorFragment){
+			templateFragments.push('<tpl if="xindex &gt; 1">');
+			templateFragments.push(this.seperatorFragment);
+			templateFragments.push('</tpl>');			
+		}
 		Ext.each(this.instanceFragments, function(fragment){
 			if(typeof fragment === 'string'){
 				templateFragments.push(fragment);
@@ -85,10 +167,12 @@ Nifty.widgets.field = Ext.extend(Ext.DataView, {
 				templateFragments.push('</span></tpl>');
 			}
 		},this);
-		templateFragments.push('</span></tpl>');
+		templateFragments.push(String.format('</{0}></tpl>', this.instanceTagName));
 
-		if(this.afterFragment){templateFragments.push(this.afterFragment)};		
-		
+		if(this.afterFragment){templateFragments.push(this.afterFragment)};	
+		templateFragments.push(String.format('<span class="x-nifty-action-tab" id="nifty-tab-{0}">',this.fieldId));	
+		templateFragments.push(this.buildActionTab());
+		templateFragments.push('</span>')
 		return templateFragments;
 	}
 });
