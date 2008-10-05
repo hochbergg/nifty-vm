aaadfd379396f03ab70ee58f0876785b2f854391
Shlomi Atar <shlomiatar@gmail.com>
2008-10-05 21:59:37 +0200

    * Documentation updates for field duplication

--- app/models/app/field/field_duplication.rb
+++ app/models/app/field/field_duplication.rb
@@ -30,27 +30,48 @@ module App
 		end
 		
 		##
-		# 
+		# Updates the duplicants for the given field instance
 		#
-		def update_duplicates!(instance)
+		# @param [Fixnum] instance_id 64bit instance_id to act upon
+		#
+		# === Notes
+		# * If the duplicant entity has changed, we need to delete the currently
+		#   duplicated instance from the older linked duplicant, and create new
+		#   duplicated instance for the new entity
+		#
+		# * If the duplicant hasn't changed, we only update the duplicated instance's
+		#   fieldlets and call #save_changes @see set_duplicants_values!
+		def update_duplicates!(instance_id)
 			if duplicant_changed? 
 				destroy_duplicates!(self.link_fieldlet.old_entity)
-				create_duplicates!(instance)
+				create_duplicates!(instance_id)
 				return
 			end
-			set_duplicants_values!(instance)
+			set_duplicants_values!(instance_id)
 			@duplicated_fieldlets.each{|f| f.new? ? f.save : f.save_changes}
 		end
 		
-		# creates a set of 
+		##
+		# Creates the duplicants for the given field instance
+		#
+		# @param [Fixnum] instance_id 64bit instance_id to act upon
+    #
+    #
 		def create_duplicates!(instance)
-			set_duplicants_values!(instance,true) # false for creating duplicates
+			set_duplicants_values!(instance,true) # true for creating duplicates
 			@duplicated_fieldlets.each{|f| f.save}
 		end
 		
-		# we want to set the duplicants values
-		# returns 
-		def set_duplicants_values!(instance, force_create = false)
+		##
+		# Updates the duplicants for the given field instance
+		#
+		# @param [Fixnum] instance_id 64bit instance_id to act upon
+		# @param [Boolean] force_create true for creating fieldlets, false for
+		#                  updating
+		#
+		#
+		 
+		def set_duplicants_values!(instance_id, force_create = false)
 			@duplicated_fieldlets = []
 			
 			fieldlets = self.all_fieldlets
@@ -69,7 +90,7 @@ module App
 				# overide the kind and the entity_id values
 				values.merge!(:entity_id => duplicant.pk, 
 											:kind => target_fieldlet_ids[i].to_i(16), 
-											:instance_id => instance)
+											:instance_id_id => instance_id)
 				
 				# if we're duplicating the link fieldlet, we need to revesrse it on the 
 				# duplicated version
@@ -77,7 +98,7 @@ module App
 					values.merge!(:int_value => @entity[:id])
 				end
 				
-				# now will create the fieldlet instances
+				# now will create the fieldlet instance_ids
 					if (fieldlet.new? || force_create)
 						target_fieldlet = ns()::Fieldlet.new
 						target_fieldlet.values.merge!(values)
@@ -93,27 +114,53 @@ module App
 		end
 		
 		
+		##
+		# Returns a list of fieldlet ids of the mirroring duplicant field
+		#
+		# @return [Array[String]] the hex encoded fieldlet ids
+		#
 		def duplicants_fieldlet_ids
 			self.duplicant_field::FIELDLET_IDS
 		end
 		
+		##
+		# Returns the Field class of the duplicant entity, which mirrors this instance field
+		# 
+		# @return [Class] the duplicant field class
+		#
+		#
 		def duplicant_field
 			@duplicant_field ||= self.class::DUPLICATION[self.duplicant.class]
 		end
 		
-		
+		##
+		# Returns the *current* linked entity to this instance
+		# The duplicant is the entity to which the link_fieldlet is pointing to *currently*
+		#
+		# @return [App::Entity] the linked entity
+		#
+		# === Notes
+		# * the @old_duplicant instance variable is filled upon loading of the entity, with the pointed
+		#   entity of the link, currently stored in the DB.
+		#
+		# * if we have no dupliant, we'll use the @old_duplicant
+		#
 		def duplicant
 			@duplicant ||= self.link_fieldlet.entity
 			@duplicant || @old_duplicant
 		end
 		
+		##
+		# Return the fieldlet with the type that was defined as the link fieldlet of the field
+		# link fieldlet connects the field to the other side of the link
+		#
+		# @return [App::Fieldlet] the linked fieldlet
+		#
 		def link_fieldlet
 			@fieldlets[self.class::LINK_FIELDLET]
 		end
 		
 		#end InstanceMethods
-	
-		
-	 #end ClassMethods	
+
 	end
 end
--- app/models/app/field/representation.rb
+++ app/models/app/field/representation.rb
@@ -1,5 +1,12 @@
 module App
 	class Field
+		##
+		# Renders to field in JSON
+		#
+		# @param [*] *args, args to pass to the to_json method
+		#
+		# @return [String] json encoded field instance
+		#
 		def to_json(*args)
 			return @fieldlets.merge({:instance => self.instance_id().to_s}).to_json(*args)
 		end
