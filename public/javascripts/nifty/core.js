d242e3eed35752dc9ed3d9d41702cbdbc667c35f
Shlomi Atar <shlomiatar@gmail.com>
2008-10-05 23:44:42 +0200

    * Added Documentation to core.js

--- public/javascripts/nifty/core.js
+++ public/javascripts/nifty/core.js
@@ -1,34 +1,45 @@
+/*
+ * Nifty-VM Ext frontend
+ * Copyright(c) 2007-2008, Shlomi Atar
+ * 
+ * http://www.niftykm.com
+ */
+
 /**
-  * Nifty Core Application
-  * by Shlomi Atar
-  */
- 
+ * Setup namespaces 
+ */
 
 Ext.namespace('Nifty.schema',
-			  'Nifty.data',
-			  'Nifty.widgets',
-			  'Nifty.pages',
-			  'Nifty.widgets.fieldlets',
-			  'Nifty.entities',
-			  'Nifty.entities.actions',
-			  'Nifty.panels',
-			  'Nifty.fields',
-			  'Nifty.layout',
-			  'Nifty.cache',
-			  'Nifty.cache.elements',
-			  'Nifty.cache.pages'
-			);
+			  			'Nifty.data',
+			  			'Nifty.widgets',
+			  			'Nifty.pages',
+			  			'Nifty.widgets.fieldlets',
+			  			'Nifty.entities',
+			  			'Nifty.entities.actions',
+			  			'Nifty.panels',
+			  			'Nifty.fields',
+			  			'Nifty.layout',
+			  			'Nifty.cache',
+			  			'Nifty.cache.elements',
+			  			'Nifty.cache.pages'
+						 );
 
 
-// create application
+/**
+ * Setup the main app code
+ * <p>The main app (Nifty.app) is called when the page has finished its loading.
+ * The main app code loads the current schema from the server, sets up the
+ * application title, register the internal url router ({@link Nifty.Router})
+ * and hides the "Loading" masks</p>
+ */
 Nifty.app = function() {
-    // do NOT access DOM from here; elements don't exist yet
- 	
-	
-    // private variables
- 
-    // private functions
+  
+	// Private
 
+  /**
+   * Hides the "loading" masks (after 250ms delay)
+	 *
+   */
 	function hideLoaders(){
 		 setTimeout(function(){
 		    	Ext.get('loading').remove();
@@ -36,33 +47,59 @@ Nifty.app = function() {
 		    }, 250);
 	}
 	
-    // public space
-    return {
-        init: function() {
+  // Public space
+  return {
+    
+		/**
+     * Starts the schema loading process and register loading events
+		 * <p>The schema id used from the viewer info, loaded in a script tag
+		 * when the page is loaded</p>
+		 * 
+		 * see {@link Nifty.schema.Loader}
+     */
+		init: function() {
 	
 			this.setStatus('Loading Schema...');
-			Nifty.schema.Loader.on('load', this.finishLoading, this);
+			Nifty.schema.Loader.on('load', this.finishSchemaLoading, this);
 			Nifty.schema.Loader.load({id: Nifty.viewerInfo.schema});
-			
 	 	},
 	
-		finishLoading: function(){
+	
+		/**
+     * A Callback called when the schema loader has finished loading the
+		 * schmea.
+		 *
+		 * <p>Hides the loaders, sets the application title and  setups the 
+		 * router</p>
+		 * 
+		 * see {@link Nifty.Router}
+     */
+		finishSchemaLoading: function(){
 			
 			// Hide Loader
 			hideLoaders();
+			
+			// Set application title
 			this.setApplicationTitle();
 			
 			// Setup the router
 			Nifty.Router.registerUrlPolling();
 		},
 		
+		/**
+     * Sets the status message of the loading indicator
+		 * @param {String} statusMessage The status message to set
+     */
 		setStatus: function(statusMessage){
 			Ext.fly('loading_status').update(statusMessage);
 		},
 		
+		/**
+     * Sets the application title according to the loaded schema
+     */
 		setApplicationTitle: function(){
 			Ext.fly('app-name').update(Nifty.schema.loaded['name']);
 		}
 	}
-}(); // end of app
+}(); // run the method and have access only to the public mehtods
  
